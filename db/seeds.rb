203d30b8d5646f1bd005399041104870716d0ea1
Mark Brooks <Markabrooks@gmail.com>
2017-07-05 20:00:58 -0600

    Assignment 16 Seeding Data: Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,17 @@ posts = Post.all
   )
 end
 
+unique_post = Post.find_or_create_by(
+  title: "This is a unique title",
+  body: "This is a unique body"
+  )
+
+Comment.find_or_create_by(
+  post: unique_post, 
+  body: "Look how unique this is!"
+  )
+
+
 puts "Seed finished!"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
