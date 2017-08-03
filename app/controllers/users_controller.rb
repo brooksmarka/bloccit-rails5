316f3a2ec4d3cb95443ab4775269017dbb9f534d
Mark Brooks <Markabrooks@gmail.com>
2017-08-03 11:50:35 -0600

    Assignment 24 Authentication: Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token
+
   def new
     @user = User.new
   end
@@ -18,5 +21,24 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+  end
+
+
+    #confirm should create a new user from the params hash and set its attributes appropriately.
+    #Send the params hash back to the create action in UsersController to save the params to the database after user confirmation.
+    #Use a button in the confirm form to achieve this:
+
+    #<%= button_to "Yes", {controller: "users", action: "create", params: params}, class: 'btn btn-success' %>
+
+    #When a user clicks the "No" button it should take them back to the initial sign up view.
+
 
 end
