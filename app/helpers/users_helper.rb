86438e2d6f2ea5d0f6e804e7a1e276004b89505a
Mark Brooks <Markabrooks@gmail.com>
2017-09-14 12:48:24 -0600

    Assignment 31 Public Profiles: Complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def user_has_post?
+    @user.posts.count >= 1
+  end
+
+  def user_has_comments?
+    @user.comments.count >=1
+  end
+
+  def user_has_favorites?
+    @user.favorites.count >=1
+  end
+
 end
